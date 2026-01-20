d9234d1778707a7e2233740ebd8d9d6f8772318e
Murilo <muriloaquinotrab@gmail.com>
2026-01-19 22:27:10 -0300

    Add files via upload

---
+++ Gestão de usuário/database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id":1, "nome": "Neymar", "email": "neyney@teste.com"},
+    {"id":2, "nome": "Freefire", "email": "dwwdasa@teste.com"},
+    {"id":3, "nome": "Loud", "email": "cuscuz2@teste.com"},
+    {"id":4, "nome": "Angolano", "email": "wasddd@teste.com"},
+    {"id":5, "nome": "Bostinha", "email": "merda@teste.com"},
+    {"id":6, "nome": "Cleber", "email": "botzinho@teste.com"},
+]
